ffd441a5250cf2660703b4009d655193ba4ffff0
takayuki nakada <mj59683@gmail.com>
2023-03-15 11:18:47 +0900

    とりあえずHello World

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>My First Page</title>
+</head>
+<body>
+<h2>Hello World!!</h2>
+<p>皆さん、こんにちは</p>
+<p>Feb/15/2021</p>
+</body>
+</html>
