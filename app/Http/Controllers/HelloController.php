de27a2dd76ae6948e4daea2ad8765d1e73818e65
takayuki nakada <mj59683@gmail.com>
2023-03-16 10:51:29 +0900

    PHPCS結果に基づいてコード修正

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -7,8 +7,8 @@ use Illuminate\Http\Request;
 class HelloController extends Controller
 {
   //
-  public function index()
-  {
-    return view('hello');
-  }
+    public function index()
+    {
+        return view('hello');
+    }
 }
